1e4edad63111c11fa7eaba205dad70bcd387663a
Sabrina <114834595+SabrinaDudaa@users.noreply.github.com>
2024-09-28 08:01:25 -0400

    Add files via upload

---
+++ JavaScript/Testes/teste.js
@@ -0,0 +1,12 @@
+var nome = 'Meu nome é Sabrina\t'
+var idade = 'Tenho 20 anos\t'
+var comida = 'Minha comida favorita é lasanha\t'
+
+var dias = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+var semana = dias[0]
+dias.push('Feriado')
+
+
+document.write("",nome, idade, comida)
+document.write("<br>")
+document.write('', semana)
